4c6c26c55c8122100d385ea4dbbc54557716c1aa
kevincoleman <kevin@kevincoleman.io>
2021-10-07 23:24:47 -0700

    make width act like inline element

--- styles.ts
+++ styles.ts
@@ -2,6 +2,7 @@ import { StyleSheet } from 'react-native'
 
 export const styles = StyleSheet.create ({
   betterButton: {
+    alignSelf: 'flex-start',
     borderRadius: 8,
     paddingTop: 12,
     paddingRight: 22,
